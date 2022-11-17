01b4356e35d3da9a33bce313bb6a16d244da9aa8
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-17 13:21:06 +0900

    updates

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Udemy: Java Programming Masterclass!");
         System.out.println("Primitive Data Types: String ");
+        System.out.println();
         //String
-        //The String is a datatype in Java, which is not a primitive type. It's actually a Class,
+        //The String is a datatype in Java, which is NOT a primitive type. It's actually a Class,
         //but it enjoys a bit of favoritism in Java to make it easier to use than a regular class.
 
         //Previous Data Types Covered
@@ -18,6 +19,16 @@ public class Main {
         8. boolean
         */
 
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + "\u00A9";
+        System.out.println("myString is equal to unicode char: " + myString);
+
+        String numberString = "250.55";
+        numberString = numberString +  "49.95";
+        System.out.printf(numberString);
 
     }
 }
