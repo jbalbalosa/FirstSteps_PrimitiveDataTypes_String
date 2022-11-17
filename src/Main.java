237c1454e063e31ad5e321f6f979bff13b8634a6
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-17 13:10:57 +0900

    updates

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Udemy: Java Programming Masterclass!");
+        System.out.println("Primitive Data Types: String ");
+        //String
+        //The String is a datatype in Java, which is not a primitive type. It's actually a Class,
+        //but it enjoys a bit of favoritism in Java to make it easier to use than a regular class.
+
+        //Previous Data Types Covered
+        /*
+        1. byte
+        2. short
+        3. int
+        4. long
+        5. float
+        6. double
+        7. char
+        8. boolean
+        */
+
+
     }
 }
